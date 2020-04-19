f8caceeee196ea8ccf0cbf604a1cb5601f8ce0be
Megan Mulroy <meganmulroy@Megans-MacBook-Air.local>
2020-04-19 16:24:56 -0400

    restructure project

---
+++ src/components/TestResult.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="finished">
+      <div>
+          <p>You answered {{ $store.getters.correct }} questions correctly out of {{ $store.getters.correct + $store.getters.incorrect}}.</p>
+          <router-link v-bind:to="{name: 'home'}" class="btn">START OVER</router-link>
+          <!-- <a class="btnSO" v-on:click.prevent="resetQuiz">START OVER</a> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+}
+</script>
+
+<style>
+  div.finished {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    width: 100%;
+    margin: auto;
+    padding: auto;
+    height: 100vh;
+    color: white;
+    background-color: darkslategrey;
+  }
+  a.btnSO{
+    height: 40px;
+    padding: 5px 30px;
+    color: darkslategrey;
+    border-radius: 10px;
+    border: 1px solid skyblue;
+    background-color: skyblue;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="start">
+      <div>
+          <p>How many addition problems can you answer correctly in 30 seconds?</p>
+          <router-link v-bind:to="{name: 'game'}" class="btn">START</router-link>
+          <!-- <a class="btn" v-on:click.prevent="timer(); newQuestion();">START</a> -->
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  props: {},
+  data() {
+    return {
+    };
+  },
+  methods: {
+      resetQuiz() {
+      this.$store.dispatch("addCorrect", -(this.$store.getters.correct));
+      this.$store.dispatch("addIncorrect", -(this.$store.getters.incorrect));
+    }
+  },
+  created() {
+    this.resetQuiz();
+  }
+};
+</script>
+
+<style>
+  div.start {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    max-width: 100%;
+    font-weight: bold;
+    color: blue;
+  }
+</style>
